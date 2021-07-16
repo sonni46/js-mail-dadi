6bba0af867887a686c39bb56645a555db8233801
Marco <sonni46marcosenpai@gmail.com>
2021-07-16 15:58:52 +0200

    fine

--- email/js/main.js
+++ email/js/main.js
@@ -5,16 +5,22 @@ var utente = prompt("Scrivi la tua email");
 
 // controlla che sia nella lista di chi può accedere,
 
-var lista = ["pippo@hotmail.it","paperino@hotmail.it"]
+var lista = ["pippo@hotmail.it","paperino@hotmail.it",]
+
+var elementoTrovato = 0
 
 for ( var i = 0; i < lista.length; i++) {
 
-    if () {
-
+    if (utente == lista[i]) {
+        alert("ben tornato");
+        elementoTrovato++
     }
-
+    else if (elementoTrovato == 0)
+    {
+       alert("errore")
+       elementoTrovato-= 2
+    }
 }
-
 
 // stampa un messaggio appropriato sull’esito del controllo.
 
