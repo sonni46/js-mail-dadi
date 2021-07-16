b68cd8d1b4a95c93c8fe3dd78cfd087010577f21
Marco <sonni46marcosenpai@gmail.com>
2021-07-16 13:52:18 +0200

    update

---
+++ email/js/main.js
@@ -0,0 +1,20 @@
+// **Mail**
+// Chiedi all’utente la sua email,
+
+var utente = prompt("Scrivi la tua email");
+
+// controlla che sia nella lista di chi può accedere,
+
+var lista = ["pippo@hotmail.it","paperino@hotmail.it"]
+
+for ( var i = 0; i < lista.length; i++) {
+
+    if () {
+
+    }
+
+}
+
+
+// stampa un messaggio appropriato sull’esito del controllo.
+
